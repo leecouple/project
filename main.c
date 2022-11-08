e7eafc7b87ec57d857152a9e2fef602a02d6b154
Haram Lee <74528522+iamraram@users.noreply.github.com>
2022-11-08 15:19:26 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,13 +4,12 @@ struct SeockChanwoo {
 	int level;
 	int height;
 	int weight;
+	char house[30];
+	char status[30];
 }user;
 
 int main() {
-	user.level = 1;
-	user.height = 180;
-	user.weight = 230;
-
-	closeTab(user.level, user.height, user.weight, "", "");
+	struct SeockChanwoo user = { 1, 180, 50, "병점역", "안녕하세요" };
+	closeTab(user.level, user.height, user.weight, user.house, user.status);
 	return 0;
 }
