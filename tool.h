52f382e04fa79ce40e98f4387421dc48905cfeba
이하람 <74528522+iamraram@users.noreply.github.com>
2022-11-13 13:20:23 +0900

    Update tool.h

--- tool.h
+++ tool.h
@@ -1,8 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+#pragma warning(disable:6262)
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
 
 extern int COLS;
 extern int LOWS;
@@ -17,6 +22,9 @@ int getColorBit(int background, int bright, int red, int green, int blue);
 
 void closeTab(int level, int height, int weight, char house[30], char status[30]);
 
-char printTitle();
+void printTitle();
+int processInput();
+int processTutorial();
 
-void makeNextTutorial();
+int makeNextTutorial();
+int ingameStart();
